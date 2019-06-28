475904f103413d31419e8e54b4007f0e787d75c8
dpark <dspark@netcrest.com>
2019-06-28 14:09:34 -0400

    Added support for Cygwin

--- hazelcast-desktop-deployment/src/main/resources/bin_sh/setenv.sh
+++ hazelcast-desktop-deployment/src/main/resources/bin_sh/setenv.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-BASE_DIR="$(dirname "$SCRIPT_DIR")"
 
-#
-# Set JAVA_HOME to the Java home (root) directory
-#
-if [ "`uname`" == "Darwin" ]; then
-   # Mac
-   export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-   #export JAVA_HOME=`/usr/libexec/java_home -v 11`
-   #export JAVA_HOME=`/usr/libexec/java_home -v 12`
-elif [ "`uname`" == "Linux" ]; then
-   export JAVA_HOME=/apps/products/jdk1.8.0_211
-else
-   export JAVA_HOME="/cygdrive/c/Program Files/Java/jdk1.8.0_211"
+# The following is for hazelcast-addon. The .addonenv.sh is
+# placed in this directory during the build time.
+if [ -f $SCRIPT_DIR/.addonenv.sh ]; then
+   . $SCRIPT_DIR/.addonenv.sh > /dev/null
 fi
 
-CODEBASE_URL=file://localhost/$BASE_DIR/
+BASE_DIR="$(dirname "$SCRIPT_DIR")"
 
-JAVA=$JAVA_HOME/bin/java
+OS_NAME=`uname`
 
-export PATH=$JAVA_HOME/bin:$PATH
+#
+# Set JAVA_HOME to the Java home (root) directory. If undefined then
+# the default java executable in PATH is used.
+#
+#if [[ ${OS_NAME^^} == DARWIN* ]]; then
+#   # Mac
+#   export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+#   #export JAVA_HOME=`/usr/libexec/java_home -v 11`
+#   #export JAVA_HOME=`/usr/libexec/java_home -v 12`
+#elif [[ ${OS_NAME^^} == CYGWIN* ]]; then
+#   export JAVA_HOME="/cygdrive/c/Program Files/Java/jdk1.8.0_212"
+#else
+#   export JAVA_HOME="/Users/dpark/Work/products/jdk1.8.0_212"
+#fi
+
+if [[ ${OS_NAME^^} == CYGWIN* ]]; then
+   __BASE_DIR="$(cygpath -wp "$BASE_DIR")"
+   BACK="\\\\"
+   FORWARD="\/"
+   CODEBASE_URL=`echo "$__BASE_DIR" | sed -e "s/$BACK/$FORWARD/g"`
+   CODEBASE_URL="file:///${CODEBASE_URL}/"
+else
+   CODEBASE_URL=file://localhost/$BASE_DIR/
+fi
+
+if [ "$JAVA_HOME" == "" ]; then
+   JAVA=java
+else
+   export PATH=$JAVA_HOME/bin:$PATH
+   JAVA=$JAVA_HOME/bin/java
+fi
 
 DESKTOP_HOME=$BASE_DIR
 NAF_HOME=$DESKTOP_HOME
@@ -34,6 +55,10 @@ if [ ! -d $LOG_DIR ]; then
 fi
 
 HAZELCAST_CLIENT_CONFIG_FILE=$DESKTOP_HOME/etc/hazelcast-client.xml 
+
+if [[ ${OS_NAME^^} == CYGWIN* ]]; then
+   HAZELCAST_CLIENT_CONFIG_FILE="$(cygpath -wp "$HAZELCAST_CLIENT_CONFIG_FILE")"
+fi
 
 JAVA_OPTS="-Xms256m -Xmx1024m -client 
 -DcodeBaseURL=$CODEBASE_URL \
@@ -51,4 +76,8 @@ NAF_JARS=$NAF_HOME/lib/naf/*
 PADO_JARS=$NAF_HOME/lib/pado/*
 DEMO_JARS=$NAF_HOME/lib/demo/*
 
-export CLASSPATH=$DESKTOP_HOME:$DESKTOP_HOME/classes:$PLUGIN_JARS:$LIB_JARS:$PADO_JARS:$NAF_JARS:$DEMO_JARS
+export CLASSPATH=$DESKTOP_HOME:$DESKTOP_HOME/classes:$PLUGIN_JARS:$LIB_JARS:$PADO_JARS:$NAF_JARS:$DEMO_JARS:$CLASSPATH
+
+if [[ ${OS_NAME^^} == CYGWIN* ]]; then
+   export CLASSPATH="$(cygpath -wp "$CLASSPATH")"
+fi
